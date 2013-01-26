aaf990e842254ebce9034716c3b0ff708170db9d
kawamuramasato <kawamuramasato@192.168.1.40>
2013-01-26 12:16:20 +0900

    add DeviceUtil

---
+++ mobiledevicereactor/DeviceUtil.py
@@ -0,0 +1,17 @@
+'''
+Created on 2013/01/26
+
+@author: kawamuramasato
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self,params):
+        '''
+        Constructor
+        '''
+        
