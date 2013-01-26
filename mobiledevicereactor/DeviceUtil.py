6a78af1c66712aa1fbbbf9eaf08ed0de6f06839f
kawamuramasato <kawamuramasato@192.168.1.40>
2013-01-26 14:06:01 +0900

    Emdn DeviceUtil.py
    define method.

--- mobiledevicereactor/DeviceUtil.py
+++ mobiledevicereactor/DeviceUtil.py
@@ -15,3 +15,27 @@ class MyClass(object):
         Constructor
         '''
         
+    def get_devices_status(self):
+        """
+        
+        :rtype: list
+        :return: [(macaddress(string),user_name(string), status(boolean))]
+        """
+        pass
+    
+    def get_registed_devices(self):
+        """
+        
+        :rtype: List
+        :return: [(macaddress(string),user_name(string))]
+        """    
+        pass
+    
+    def get_registed_device(self):
+        """
+        
+        :rtype: tuple
+        :return: (macaddress(string), user_name(string))
+        """
+        
+        pass
