bfff2323b9762f95c23fd18dbfdb64685f1f438f
kawamuramasato <kawamuramasato@192.168.1.40>
2013-01-26 12:18:29 +0900

    add actionexecutor and action

---
+++ mobiledevicereactor/Action.py
@@ -0,0 +1,17 @@
+'''
+Created on 2013/01/26
+
+@author: kawamuramasato
+'''
+
+class Action(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self,params):
+        '''
+        Constructor
+        '''
+        
---
+++ mobiledevicereactor/ActionExecutor.py
@@ -0,0 +1,17 @@
+'''
+Created on 2013/01/26
+
+@author: kawamuramasato
+'''
+
+class ActionExecutor(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self,params):
+        '''
+        Constructor
+        '''
+        
